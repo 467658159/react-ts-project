37b8de96d307f066887b9b47df2d15c7660d1e32
steez <467658159@qq.com>
2022-03-02 11:13:02 +0800

    修改prettier配置

--- hello.ts
+++ hello.ts
@@ -1,4 +1,4 @@
-const add = (a, b) => {
+const add = (a: number, b: string) => {
   console.log(a + b)
   return a + b
 }
---
+++ scripts/constants.js
@@ -0,0 +1,12 @@
+const isDev = process.env.NODE_ENV !== 'production'
+
+const path = require('path')
+
+const PROJECT_PATH = path.resolve(__dirname, '../')
+const PROJECT_NAME = path.parse(PROJECT_PATH).name
+
+module.exports = {
+  isDev,
+  PROJECT_PATH,
+  PROJECT_NAME,
+}
